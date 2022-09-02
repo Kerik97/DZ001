a3471236350f5d922e40f7826ba0f43c04c03ece
kirill <k.grinikh@gmail.com>
2022-09-03 00:42:31 +0300

    task004

---
+++ task004/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 1)
+{
+    Console.WriteLine("False");
+}
+else
+{
+    Console.WriteLine("True");
+}
