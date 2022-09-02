5c198f6395b775cd2c9e48268fcd4952a1766cc3
kirill <k.grinikh@gmail.com>
2022-09-03 00:46:53 +0300

    task006

---
+++ task006/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Введите число ");
+string num = Console.ReadLine();
+int y = num[num.Length - 1] - '0';
+int n = y * y;
+
+Console.WriteLine("Последняя цифра " + y);
